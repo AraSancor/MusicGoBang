691aa6af7ca1845618df01cd9483444662782d33
yess <yessjobsinf@gmail.com>
2022-05-03 16:01:20 +0200

    WebRa

---
+++ js/historia.js
@@ -0,0 +1,138 @@
+function mostrarHistoria() {
+
+    fecha = new Date();
+    hora = fecha.getHours();
+
+    if (hora >= 0 && hora < 1) {
+      texto = "ANTIDOTE N.Y.C. - Viva Los Pendejos, LP (Active Records, 1992) Tercer disco de la banda de NYHC, incluye las sesiones «Keep The Faith» y «Return 2 Burn» en la versión Cd se incluyen bonus tracks en directo. ";
+      imagen = "img2/AntidoteNyc.jpg";
+    }
+
+    if (hora >= 1 && hora < 2) {
+      texto = "BERLIN “Pleasure Victim” LP (Mercury, 1982) Segundo disco de la banda de new wave, conocida internacionalmente por su éxito “Take My Breath Away”.";
+      imagen = "img2/berlin.jpg";
+    }
+
+    if (hora >= 2 && hora < 3) {
+      texto = "CHEMICAL PEOPLE “Overdosed On” LP (Vinyl Solution, 1990) Recopilatorio que incluye todos los singles editados por la banda californiana en su propio sello, 2” Pecker Records. Portada a cargo de Ray Ahn de Hard-Ons. ";
+      imagen = "img2/chemicalpeople.jpg";
+    }
+
+
+    if (hora >= 3 && hora < 4) {
+      texto = "CHOIR INVISIBLE “Choir Invisible” LP (Frontier Records, 1991) Debut de esta banda de post-punk surgida de las cenizas de The Flyboys.";
+      imagen = "img2/choirinvisible.jpg";
+    }
+
+    if (hora >= 4 && hora < 5) {
+      texto = "DESTROY ALL MONSTERS “Hot Box 1974-1994” 3LP (Munster Records, 2014) Incluye un Lp con los singles, un Lp en directo y otro con material experimental. Con Niagara, Michael Davis (MC5) y Ron Asheton (The Stooges).";
+      imagen = "img2/destroyallmonsters.jpg";
+    }
+
+    if (hora >= 5 && hora < 6) {
+      texto = "EDDIE AND THE HOT RODS “Life On The Line” LP (Island Records, 1977) Segundo album de la banda liderada por Barrie Masters, con clásicos como “Do Anything You Wanna Do”, “Quit This Town” o “Ignore Them”.";
+      imagen = "img2/eddieanthehotrods.jpg";
+    }
+    if (hora >= 6 && hora < 7) {
+      texto = "FARSIDE “Rigged” LP, (Revelation Records, 1994) Segundo album de esta genial banda de HC Melódico de California, en sus filas estuvo Zach De La Rocha de Rage Against The Machine.";
+      imagen = "img2/farside.jpg";
+    }
+
+    if (hora >= 7 && hora < 8) {
+      texto = "GANG GREEN “King Of Bands” Lp, (Roadrunner Records, 1991) Recopilatorio de esta genial banda de Hardcore de Boston.";
+      imagen = "img2/ganggreen.jpg";
+    }
+
+    if (hora >= 8 && hora < 9) {
+      texto = "HOODOO GURUS “Magnum Cum Louder” LP, (RCA, 1989) Cuarto disco de esta banda australiana, com miembros de The Victims, Fun Things y The Hitmen.";
+      imagen = "img2/hoodoogurus.jpg";
+    }
+
+
+    if (hora >= 9 && hora < 10) {
+      texto = "JEFF McDONALD “Jeff McDonald” LP, (Bang Records, 2016) Primer disco en solitario de Jeff McDonald,después de la separación de Redd Kross en 1997, y del cual solo se editaron unas 10 o 15 copias en cd.";
+      imagen = "img2/jeffmcdonald.jpg";
+    }
+
+    if (hora >= 10 && hora < 11) {
+      texto = "KOMPLIKATIONS “Step Forward” Mini-Lp, (Rockstar Records, 2019) Reedición del primer Mini-Lp de este genial trio de Synth-Punk de Bélgica, editado originalmente en 2012.";
+      imagen = "img2/komplikationsstepforward.jpg";
+    }
+
+    if (hora >= 11 && hora < 12) {
+      texto = "L7 “Smell The Magic” Mini-Lp, (Sup Pop Records, 1990) Segunda referencia de este genial grupo de riot-girls, el primero en Sub-Pop.";
+      imagen = "img2/L7.jpg";
+    }
+    
+    if (hora >= 12 && hora < 13) {
+      texto = "MAGAZINE “Real Life” LP, (Virgin Records, 1979) Primer disco de la banda formada por Howard Devoto después de dejar The Buzcocks. ";
+      imagen = "img2/magazine.jpg";
+    }
+
+
+    if (hora >= 13 && hora < 14) {
+      texto = "NAKED RAYGUY “Understand?” LP, (Caroline Records, 1989) Cuarto album de esta banda de punk de Chicago, imprescindible.";
+      imagen = "img2/nakedraygun.jpg";
+    }
+
+    if (hora >= 14 && hora < 15) {
+      texto = "OUTSIDERS “Close Up” LP (Raw Edge Records, 1978) Segundo album del grupo de Adrian Borland y Graham Bailey antes de formar The Sound.";
+      imagen = "img2/outsiders.jpg";
+    }
+
+    if (hora >= 15 && hora < 16) {
+      texto = "PAGANS “Shit Street” LP, (Crypt Records, 2001) Segundo album del grupo de Adrian Borland y Graham Bailey antes de formar The Sound. ";
+      imagen = "img2/pagans.jpg";
+    }
+
+
+    if (hora >= 16 && hora < 17) {
+      texto = "RED CROSS “Born Innocent” LP, (Smoke Seven, 1982) Primer album del grupo de los hermanos Jeff y Steven McDonald, al poco tiempo se tuvieron que cambiar el nombre para evitar una demanda de la Cruz Roja, IMPRESCINDIBLE.";
+      imagen = "img2/redcross.jpg";
+    }
+
+
+    if (hora >= 17 && hora < 18) {
+      texto = "SACRED REICH “Surf Nicaragua” Mini-LP, (Metal Blade, 2021) Reedición del clásico mini-lp editado originalmente en 1988";
+      imagen = "img2/sacredreich.jpg";
+    }
+
+    if (hora >= 18 && hora < 19) {
+      texto = "EDDIE AND THE HOT RODS “Life On The Line” LP (Island Records, 1977) Segundo album de la banda liderada por Barrie Masters, con clásicos como “Do Anything You Wanna Do”, “Quit This Town” o “Ignore Them”.";
+      imagen = "img2/eddieandthehotrods.jpg";
+    }
+
+    if (hora >= 19 && hora < 20) {
+      texto = "FARSIDE “Rigged” LP, (Revelation Records, 1994) Segundo album de esta genial banda de HC Melódico de California, en sus filas estuvo Zach De La Rocha de Rage Against The Machine.";
+      imagen = "img2/farside.jpg";
+    }
+
+
+    if (hora >= 20 && hora < 21) {
+      texto = "GANG GREEN “King Of Bands” Lp, (Roadrunner Records, 1991) Recopilatorio de esta genial banda de Hardcore de Boston.";
+      imagen = "img2/ganggreen.jpg";
+    }
+
+
+    if (hora >= 21 && hora < 22) {
+      texto = "HOODOO GURUS “Magnum Cum Louder” LP, (RCA, 1989) Cuarto disco de esta banda australiana, com miembros de The Victims, Fun Things y The Hitmen.";
+      imagen = "img2/hoodoogurus.jpg";
+    }
+
+    if (hora >= 22 && hora < 23) {
+      texto = "JEFF McDONALD “Jeff McDonald” LP, (Bang Records, 2016) Primer disco en solitario de Jeff McDonald,después de la separación de Redd Kross en 1997, y del cual solo se editaron unas 10 o 15 copias en cd.";
+      imagen = "img2/jeffmcdonald.jpg";
+    }
+
+    if (hora >= 23 && hora < 24) {
+      texto = "KOMPLIKATIONS “Step Forward” Mini-Lp, (Rockstar Records, 2019) Reedición del primer Mini-Lp de este genial trio de Synth-Punk de Bélgica, editado originalmente en 2012.";
+      imagen = "img2/komplikationsstepforward.jpg";
+    }
+
+    document.images["tiempo"].src = imagen;
+
+    document.getElementById('txt').innerHTML = texto;
+
+  }
+
+  mostrarHistoria();
---
+++ js/history.js
@@ -0,0 +1,138 @@
+function mostrarHistoria() {
+
+    fecha = new Date();
+    hora = fecha.getHours();
+
+    if (hora >= 0 && hora < 1) {
+      texto = "CHICHLIDS “Be True To Your School” LP (T.K. Records, 1980), The only album by this interesting group from Florida, where they mix Pop, Punk, New Wave and Surf.";
+      imagen = "img2/chichlids.jpg";
+    }
+
+    if (hora >= 1 && hora < 2) {
+      texto = "DEXYS MIDNIGHT RUNNERS “Geno” LP (EMI, 1983), Compilation of some of their first singles (1978 to 1981) of this great well-known band especially for his success Come On Eileen.";
+      imagen = "img2/dexysmidnightrunners.jpg";
+    }
+
+    if (hora >= 2 && hora < 3) {
+      texto = "EFFIGIES “Ink” LP (Restless Records, 1986) Third album by this great punk band from Chicago formed in 1979.";
+      imagen = "img2/effigiesink.jpg";
+    }
+
+    if (hora >= 3 && hora < 4) {
+      texto = "FEAR “The Record” LP, (Slash Records, 2007) Reissue of the first FEAR album, an American punk classic released in 1982. ";
+      imagen = "img2/fear.jpg";
+    }
+
+    if (hora >= 4 && hora < 5) {
+      texto = "Hüsker Dü “Zen Arcade” 2LP (SST, 1984) Hüsker Dü's third album, where they lay the foundations for alternative rock.";
+      imagen = "img2/huskerduzenarcade.jpg";
+    }
+
+    if (hora >= 5 && hora < 6) {
+      texto = "JELLO BIAFRA with DOA “Last Scream Of The Missing Neighbors” Lp, (Alternative Tentacles, 1989) Brutal collaboration between the leader of Dead Kennedys and DOA, great cover by Winston Smith.";
+      imagen = "img2/jellobiafraedoa.jpg";
+    }
+
+    if (hora >= 6 && hora < 7) {
+      texto = "KURT BAKER COMBO “Let ́s Go Wild” LP, (Wicked Cool Records, 2018) Third album by the versatile Kurt Baker in his Spanish adventure. VERY COOL!";
+      imagen = "img2/kurtbakercombo.jpg";
+    }
+
+    if (hora >= 7 && hora < 8) {
+      texto = "LAST “L.A. Explosion”, LP (Bomp Records, 1979) Wonderful first album by The Last, a masterpiece of Californian Power-Pop.";
+      imagen = "img2/last.jpg";
+    }
+
+    if (hora >= 8 && hora < 9) {
+      texto = "MANITOBA WILD KINGDOM “...And You?” LP, (MCA Records, 1990) The return of The Dictators, changing some components and speeding up their characteristic sound.";
+      imagen = "img2/manitobawildkingdom.jpg";
+    }
+
+
+    if (hora >= 9 && hora < 10) {
+      texto = "NEGATIVE APPROACH “Pressure” LP, (Recollect Records, 2012) Unofficial disc that includes his album “Tied Down” on one side and songs recorded live in 1983 in the other..";
+      imagen = "img2/negativeapproach.jpg";
+    }
+
+    if (hora >= 10 && hora < 11) {
+    
+      texto = "PATTI SMITH “Horses” LP, (EMI, 1980) Spanish reissue of the first album of the “punk poet”..";
+      imagen = "img2/pattismith.jpg";
+
+    }
+
+    if (hora >= 11 && hora < 12) {
+      texto = "R.E.M. “Bodycount At Tyrone's” LP, (Brigand, 1990) Unofficial live album recorded in Athens (GA) on October 8, 1980, most of these songs were never recorded in the studio. ";
+      imagen = "img2/rem.jpg";
+    }
+
+    if (hora >= 12 && hora < 13) {
+      texto = "SCRAPS “Wrapped Up In This Society” LP, (Be Yourself Records, 1990) First album of this classic Hardcore Punk band from Belgium. ";
+      imagen = "img2/scraps.jpg";
+    }
+
+    if (hora >= 13 && hora < 14) {
+      texto = "Hüsker Dü “Zen Arcade” 2LP (SST, 1984) Hüsker Dü's third album, where they lay the foundations for alternative rock.";
+      imagen = "img2/huskerduzenarcade.jpg";
+    }
+
+    if (hora >= 14 && hora < 15) {
+      texto = "JELLO BIAFRA with DOA “Last Scream Of The Missing Neighbors” Lp, (Alternative Tentacles, 1989) Brutal collaboration between the leader of Dead Kennedys and DOA, great cover by Winston Smith.";
+      imagen = "img2/jellobiafraedoa.jpg";
+    }
+
+    if (hora >= 15 && hora < 16) {
+      texto = "KURT BAKER COMBO “Let ́s Go Wild” LP, (Wicked Cool Records, 2018) Third album by the versatile Kurt Baker in his Spanish adventure. VERY COOL!";
+      imagen = "img2/kurtbakercombo.jpg";
+    }
+
+    if (hora >= 16 && hora < 17) {
+      texto = "LAST “L.A. Explosion”, LP (Bomp Records, 1979) Wonderful first album by The Last, a masterpiece of Californian Power-Pop.";
+      imagen = "img2/last.jpg";
+    }
+
+    if (hora >= 17 && hora < 18) {
+      texto = "R.E.M. “Bodycount At Tyrone's” LP, (Brigand, 1990) Unofficial live album recorded in Athens (GA) on October 8, 1980, most of these songs were never recorded in the studio. ";
+      imagen = "img2/rem.jpg";
+    }
+
+
+    if (hora >= 18 && hora < 19) {
+      texto = "MANITOBA WILD KINGDOM “...And You?” LP, (MCA Records, 1990) The return of The Dictators, changing some components and speeding up their characteristic sound.";
+      imagen = "img2/manitobawildkingdom.jpg";
+    }
+
+    if (hora >= 19 && hora < 20) {
+      texto = "EFFIGIES “Ink” LP (Restless Records, 1986) Third album by this great punk band from Chicago formed in 1979.";
+      imagen = "img2/effigiesink.jpg";
+    }
+
+    if (hora >= 20 && hora < 21) {
+      texto = "CHICHLIDS “Be True To Your School” LP (T.K. Records, 1980), The only album by this interesting group from Florida, where they mix Pop, Punk, New Wave and Surf.";
+      imagen = "img2/chichlids.jpg";
+    }
+
+    if (hora >= 21 && hora < 22) {
+      texto = "SCRAPS “Wrapped Up In This Society” LP, (Be Yourself Records, 1990) First album of this classic Hardcore Punk band from Belgium. ";
+      imagen = "img2/scraps.jpg";
+    }
+
+
+    
+    if (hora >= 22 && hora < 23) {
+      texto = "KURT BAKER COMBO “Let ́s Go Wild” LP, (Wicked Cool Records, 2018) Third album by the versatile Kurt Baker in his Spanish adventure. VERY COOL!";
+      imagen = "img2/kurtbakercombo.jpg";
+    }
+
+    if (hora >= 23 && hora < 24) {
+      texto = "JELLO BIAFRA with DOA “Last Scream Of The Missing Neighbors” Lp, (Alternative Tentacles, 1989) Brutal collaboration between the leader of Dead Kennedys and DOA, great cover by Winston Smith.";
+      imagen = "img2/jellobiafraedoa.jpg";
+    }
+
+    document.images["tiempo"].src = imagen;
+
+    document.getElementById('txt').innerHTML = texto;
+
+  }
+
+  mostrarHistoria();
